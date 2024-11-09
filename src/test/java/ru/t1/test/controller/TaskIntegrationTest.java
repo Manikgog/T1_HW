935cadc53d0d16a04d0cd1077e75d17eecf57781
Максим Гоголин <maxinata@rambler.ru>
2024-11-09 10:39:08 +0500

    Написал методы для тестирования метода get TaskController.

--- src/test/java/ru/t1/test/controller/TaskIntegrationTest.java
+++ src/test/java/ru/t1/test/controller/TaskIntegrationTest.java
@@ -1,17 +1,26 @@
 package ru.t1.test.controller;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.t1.test.PostgresSQLTestContainerExtension;
+import ru.t1.test.TaskStatus;
 import ru.t1.test.repository.TaskRepository;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.t1.test.util.TestData.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class TaskIntegrationTest {
+public class TaskIntegrationTest extends PostgresSQLTestContainerExtension {
 
     @Autowired
     private MockMvc mockMvc;
@@ -22,10 +31,23 @@ public class TaskIntegrationTest {
     @BeforeEach
     void setUp() {
         taskRepository.deleteAll();
-        taskRepository.save(TASK_1);
-        taskRepository.save(TASK_2);
-        taskRepository.save(TASK_3);
-        taskRepository.save(TASK_4);
+        taskRepository.saveAll(List.of(TASK_1, TASK_2, TASK_3, TASK_4));
+    }
+
+    @Test
+    void getPositiveTest() throws Exception {
+        mockMvc.perform(get(URL_TEMPLATE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title", is(TASK_1.getTitle())))
+                .andExpect(jsonPath("$.description", is(TASK_1.getDescription())))
+                .andExpect(jsonPath("$.status", is(TaskStatus.RUNNING.toString())));
+    }
+
+    @Test
+    void getNegativeTest() throws Exception {
+        mockMvc.perform(get(String.format("/tasks/%s", WRONG_TASK_ID)))
+                .andExpect(status().isBadRequest());
     }
 
 
